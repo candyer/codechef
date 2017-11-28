66a35a45e8a018b7a027ea0708b4a29bd58d31f1
cc <candyer@users.noreply.github.com>
2017-11-28 23:39:42 +0100

    lunchtime

---
+++ November Lunchtime 2017/smrstr/smrstr.py
@@ -0,0 +1,28 @@
+# https://www.codechef.com/LTIME54/problems/SMRSTR
+
+# brute force
+# def solve(n, q, array, query):
+# 	res = []
+# 	for x in query:
+# 		for num in array:
+# 			x /= num
+# 		res.append(str(x))
+# 	return ' '.join(res)
+
+def solve(n, q, array, query):
+	tmp = 1
+	for num in array:
+		tmp *= num
+	res = []
+	for x in query:
+		res.append(str(x / tmp))
+	return ' '.join(res)
+
+if __name__ == '__main__':
+	t = int(raw_input())
+	for _ in range(t):
+		n, q = map(int, raw_input().split())
+		array = map(int, raw_input().split())
+		query = map(int, raw_input().split())
+		print solve(n, q, array, query)
+
