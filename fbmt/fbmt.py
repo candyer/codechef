d872b459fd08aa5f861b0bbd38715333626cb3c3
cc <candyer@users.noreply.github.com>
2017-12-24 23:29:40 +0100

    December Cook-Off 2017 - Football Match

---
+++ fbmt/fbmt.py
@@ -0,0 +1,32 @@
+# https://www.codechef.com/problems/FBMT
+
+# def solve(n, s):
+# 	return n, s
+
+from collections import defaultdict
+
+if __name__ == '__main__':
+	t = int(raw_input())
+	for _ in range(t):
+		d = defaultdict(int)
+		n = int(raw_input())
+		for i in range(n):
+			s = raw_input()
+			d[s] += 1
+		if len(d) == 0:
+			print 'Draw'
+		elif len(d) == 1:
+			print d.keys()[0]
+		else:
+			team1, team2 = d
+			if d[team1] == d[team2]:
+				print 'Draw'
+			elif d[team1] > d[team2]: 
+				print team1
+			else:
+				print team2
+
+
+
+
+
