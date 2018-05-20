0d37bdb621eca89333b2af8dc2e94d734397496a
cc <candyer@users.noreply.github.com>
2018-05-20 21:47:41 +0200

    Best Cake Ever

--- May Cook-off 2018/kmxor/kmxor.py
+++ May Cook-off 2018/kmxor/kmxor.py
@@ -1,47 +1,6 @@
 
 import math
 def solve(n, k):
-	if n == 1:
-		return ' '.join([str(k)])
-	if k == 1:
-		return ' '.join(['1'] * n)
-	if k == 2:
-		return ' '.join(['1', '2'] + ['1'] * (n - 2))
-	if k == 3:
-		if n % 2 == 1:
-			return (' '.join(['3'] * n))
-		else:
-			return (' '.join(['1'] * (n - 1) + ['2']))
-	res = []
-	tmp = int(math.log(k, 2) + 1)
-
-	if n % 2 == 0:
-		x = 2 ** (tmp - 1)
-		y = x - 1
-		res.extend([str(x), str(y)])
-		
-	else:
-		pass
-		x = int('1'*(tmp - 1), 2) + 1
-		y = int('1'*(tmp - 2) + '0', 2)
-		res.extend([str(x), str(y)])
-	res.extend(['1']*(n - 2))
-	return ' '.join(res)
-
-
-# if __name__ == '__main__':
-# 	t = int(raw_input())
-# 	for _ in range(t):
-# 		n, k = map(int, raw_input().split())
-# 		print solve(n, k)
-
-
-# print solve(6, 10)
-
-
-
-import math
-def solve(n, k):
 
 	if n == 1:
 		return k
